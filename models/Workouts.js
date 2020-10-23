1fdf9d53cc78bc9dea8e4eb0df1bb1d35a558446
Zach Drummond <zachdrummond3@gmail.com>
2020-10-23 14:23:02 -0400

    Added virtual to workout schema

--- models/Workouts.js
+++ models/Workouts.js
@@ -35,7 +35,16 @@ const WorkoutSchema = new Schema(
       },
     ],
   },
+  {
+    toJSON: { virtuals: true }
+  }
 );
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
